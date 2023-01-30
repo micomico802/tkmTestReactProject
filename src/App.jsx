3d6231331490db8605272190266b8b70aee12369
micomico802 <sho.mizumoto802@gmail.com>
2023-01-30 19:12:48 +0900

    一旦masterブランチに追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,19 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GreenPage from "./pages/GreenPage";
 import YellowPage from "./pages/YellowPage";
 import RedPage from "./pages/RedPage";
-import Button from "./components/Button";
 
 const App = () => {
-  const [pageChange, setPageChange] = useState("green");
-
   return (
-    <>
-     <GreenPage />
-      <div className="Button-Container">
-        <Button
-          buttonName="赤エリア"
-          onClickEvent={() => setPageChange("red")}
-        />
-        <Button
-          buttonName="黄色エリア"
-          onClickEvent={() => setPageChange("yellow")}
-        />
-        <Button
-          buttonName="緑エリア"
-          onClickEvent={() => setPageChange("green")}
-        />
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path={`/`} element={<GreenPage />} />
+        <Route path={`/yellow/`} element={<YellowPage />} />
+        <Route path={`/red/`} element={<RedPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
 export default App;
+
